fb2f129e7bb77214f181c97c571b4348fffced1e
Jared Hoberock <jaredhoberock@gmail.com>
2020-05-05 15:12:11 -0500

    Introduce and test set_done

---
+++ tests/sender/set_done.hpp
@@ -0,0 +1,67 @@
+#include <cassert>
+#include <cudex/sender/set_done.hpp>
+
+
+#ifndef __CUDACC__
+#define __host__
+#define __device__
+#endif
+
+
+struct has_set_done_member
+{
+  __host__ __device__
+  bool set_done() && noexcept
+  {
+    return true;
+  }
+};
+
+
+struct has_set_done_free_function {};
+
+__host__ __device__
+bool set_done(has_set_done_free_function&&) noexcept
+{
+  return true;
+}
+
+
+__host__ __device__
+void test()
+{
+  {
+    has_set_done_member r;
+
+    assert(cudex::set_done(std::move(r)));
+  }
+
+  {
+    has_set_done_free_function r;
+
+    assert(cudex::set_done(std::move(r)));
+  }
+}
+
+#ifdef __CUDACC__
+template<class F>
+__global__ void device_invoke(F f)
+{
+  f();
+}
+#endif
+
+
+void test_set_done()
+{
+  test();
+
+#ifdef __CUDACC__
+  device_invoke<<<1,1>>>([] __device__ ()
+  {
+    test();
+  });
+  assert(cudaDeviceSynchronize() == cudaSuccess);
+#endif
+}
+
