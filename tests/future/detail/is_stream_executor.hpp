b1e2489b285e94cfbdb720710ba2ed5f0e4d7c38
Jared Hoberock <jaredhoberock@gmail.com>
2020-05-26 16:47:03 -0500

    Eliminate debug code

--- tests/future/detail/is_stream_executor.hpp
+++ tests/future/detail/is_stream_executor.hpp
@@ -6,14 +6,7 @@ namespace ns = cusend;
 
 void test_is_stream_executor()
 {
-//  static_assert(ns::is_applicable_property<ns::execution::stream_executor, ns::execution::stream_property>::value, "Error.");
-  static_assert(ns::execution::stream_property::is_applicable_property<ns::execution::stream_executor>(), "error");
-
-  ns::execution::stream_executor ex;
-  query(ex, ns::execution::stream);
-//  ns::query(ex, ns::execution::stream);
-//  static_assert(ns::can_query<ns::execution::stream_executor, ns::execution::stream_property>::value, "Error.");
-//  static_assert(ns::detail::is_stream_executor<ns::execution::stream_executor>::value, "Error.");
-//  static_assert(ns::detail::is_stream_executor<ns::execution::callback_executor>::value, "Error.");
+  static_assert(ns::detail::is_stream_executor<ns::execution::stream_executor>::value, "Error.");
+  static_assert(ns::detail::is_stream_executor<ns::execution::callback_executor>::value, "Error.");
 }
 
