cf106d98d1a1b210f1b542dc29a27693a5e3c266
Jared Hoberock <jaredhoberock@gmail.com>
2020-05-27 15:40:03 -0500

    Introduce detail::then_execute

---
+++ cusend/future/detail/then_execute.hpp
@@ -0,0 +1,111 @@
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+#include "../../detail/prologue.hpp"
+
+#include <future>
+#include <type_traits>
+#include "../../detail/type_traits/is_invocable.hpp"
+#include "../../detail/event.hpp"
+#include "../../execution/executor/callback_executor.hpp"
+#include "../../execution/executor/execute.hpp"
+#include "is_stream_executor.hpp"
+#include "stream_of.hpp"
+
+
+CUSEND_NAMESPACE_OPEN_BRACE
+
+
+namespace detail
+{
+
+
+template<class T, class Function,
+         CUSEND_REQUIRES(is_invocable<Function&&,T&&>::value),
+         CUSEND_REQUIRES(std::is_void<invoke_result_t<Function&&,T&&>>::value)
+        >
+detail::event then_execute(const execution::callback_executor& ex, std::future<T>&& predecessor, Function&& f)
+{
+  ex.execute([fut = std::move(predecessor), f = std::move(f)]() mutable
+  {
+    f(fut.get());
+  });
+
+  // return an event recorded on ex's stream
+  return {ex.stream()};
+}
+
+
+template<class Function,
+         CUSEND_REQUIRES(is_invocable<Function&&>::value),
+         CUSEND_REQUIRES(std::is_void<invoke_result_t<Function&&>>::value)
+        >
+detail::event then_execute(const execution::callback_executor& ex, std::future<void>&& predecessor, Function&& f)
+{
+  ex.execute([fut = std::move(predecessor), f = std::move(f)]() mutable
+  {
+    fut.get();
+    f();
+  });
+
+  // return an event recorded on ex's stream
+  return {ex.stream()};
+}
+
+
+template<class StreamExecutor,
+         class Function,
+         CUSEND_REQUIRES(is_stream_executor<StreamExecutor>::value),
+         CUSEND_REQUIRES(is_invocable<Function>::value),
+         CUSEND_REQUIRES(std::is_void<invoke_result_t<Function>>::value)
+        >
+event then_execute(const StreamExecutor& ex, event&& e, Function f)
+{
+  // get ex's stream
+  cudaStream_t stream = detail::stream_of(ex);
+
+  // make stream wait on the predecessor event
+  detail::stream_wait_for(stream, e.native_handle());
+
+  // execute f
+  execution::execute(ex, f);
+
+  // re-record the event on the stream
+  e.record_on(stream);
+
+  return std::move(e);
+}
+
+
+} // end detail
+
+
+CUSEND_NAMESPACE_CLOSE_BRACE
+
+#include "../../detail/epilogue.hpp"
+
